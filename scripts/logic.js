d233fc984f54f68e7392d4b0f05510dfa624e260
azeem2504 <118335209+azeem2504@users.noreply.github.com>
2024-06-29 14:02:42 +0530

    Update logic.js

--- scripts/logic.js
+++ scripts/logic.js
@@ -1,97 +1,97 @@
-window.addEventListener('load', bindEvents)
-var flag = true;
-var count = 0;
+window.addEventListener('load', bindEvents);
 var buttons;
-var btn;
+var flag = true
+var count = 0
 var out;
+var btn;
+
+
 function bindEvents(){
     buttons = document.getElementsByTagName('button')
-    for(var i = 0; i<buttons.length; i++){
-        buttons[i].addEventListener('click', printXorZero)
+    for(var i = 0; i < buttons.length; i++){
+        buttons[i].addEventListener('click', XorZero);
     }
-    btn = document.getElementById('reset')
-    btn.addEventListener('click', reset)
+    document.getElementById('reset').addEventListener('click', reset5sec)
 }
-//maintain a counter to check if count goes > 4 then heck game over conditions
+
 function isGameOver(){
-    //in gameover conditions 
-    //1. row value are same
-    //2. column value arw same
-    //3. diagonal value are same
-    //click shouldnt work after gameover
-
-
     out = document.getElementById('output')
-    if(count > 4){
-        //row check
-        for (var i = 0; i < 9; i += 3) {
-            if(buttons[i].innerText && buttons[i].innerText == buttons[i+1].innerText && buttons[i].innerText == buttons[i+2].innerText){
-                // alert(`Game Over WINNER is ${buttons[i].innerText}`)
-                out.innerText = `Game Over WINNER is ${buttons[i].innerText}`
-                
-                disable()
-            }
-        }
-        
-        //column check
-        for (var i = 0; i < 3; i++){
-            if(buttons[i].innerText && buttons[i].innerText == buttons[i+3].innerText && buttons[i].innerText == buttons[i+6].innerText){
-                // alert(`Game Over WINNER is ${buttons[i].innerText}`)
-                out.innerText = `Game Over WINNER is ${buttons[i].innerText}`
-                disable()
-            }
-        }
-        //diagonal check
-        if(buttons[0].innerText && buttons[0].innerText == buttons[4].innerText && buttons[0].innerText == buttons[8].innerText){
-            // alert(`Game Over WINNER is ${buttons[0].innerText}`)
-            out.innerText = `Game Over WINNER is ${buttons[0].innerText}`
+    for(var i = 0; i < 9; i+=3){
+        if(buttons[i].innerText && buttons[i].innerText == buttons[i+1].innerText && buttons[i].innerText == buttons[i+2].innerText){
+            out.innerText = `Game Over WINNER is ${buttons[i].innerText}`
             disable()
+            return true
         }
-        
-        if(buttons[2].innerText && buttons[2].innerText == buttons[4].innerText && buttons[2].innerText == buttons[6].innerText){
-            // alert(`Game Over WINNER is ${buttons[2].innerText}`)
-            out.innerText = `Game Over WINNER is ${buttons[2].innerText}`
-            disable()
-        }
-
-        
-        
     }
-    //draw
-    if(count > 9){
-        // alert(`Game Draw`)
-        out.innerText = "Game Draw"
+    for(var i = 0; i < 3; i++){
+        if(buttons[i].innerText && buttons[i].innerText == buttons[i+3].innerText && buttons[i].innerText == buttons[i+6].innerText){
+            out.innerText = `Game Over WINNER is ${buttons[i].innerText}`
+            disable()
+            return true
+        }
+    }
+    if(buttons[0].innerText && buttons[0].innerText == buttons[4].innerText && buttons[0].innerText == buttons[8].innerText){
+        out.innerText = `Game Over WINNER is ${buttons[0].innerText}`
         disable()
+        return true
     }
+    if(buttons[2].innerText && buttons[2].innerText == buttons[4].innerText && buttons[2].innerText == buttons[6].innerText){
+        out.innerText = `Game Over WINNER is ${buttons[2].innerText}`
+        disable()
+        return true
+    }
+    return false
+    
+
 }
+
 function disable(){
-    for(var i = 0 ;i < buttons.length; i++){
+    for(var i = 0; i < buttons.length; i++){
         buttons[i].disabled = true;
-        btn.disabled = false
     }
+    document.getElementById('reset').disabled = false;
+
 }
 
-// function setTime(){
-//     setTimeout(function(){
-//         reset();
-//     }, 5000);
-// }
+var countDownValue = 5
+var interval;
+function countDown(){
+    interval = setInterval(function(){
+        countDownValue--;
+        out.innerText = `Game Reset in ${countDownValue} Sec`
+    },1000)
+}
 
+function reset5sec(){
+    countDown()
+    setTimeout(function(){
+        reset()
+    }, 5000)
+}
 function reset(){
-    for(var i = 0; i < buttons.length; i++){ 
-        buttons[i].disabled = false;
-        buttons[i].innerText = "";
+    clearInterval(interval)
+    countDownValue = 5
+    count = 0
+    for(var i = 0; i < buttons.length; i++){
+        buttons[i].innerText = ""
+        buttons[i].disabled = false
     }
-    btn.innerText = "RESET"
+    document.getElementById('reset').innerText = "RESET"
     out.innerText = ""
-    count = 0;
+    flag = true
 }
-function printXorZero(){
+
+function XorZero(){
     if(this.innerText.trim().length == 0){
+        count++;
         var buttonValue = flag?"X":"0"
         this.innerText = buttonValue
         flag = !flag
-        count++;
-        isGameOver()
+        if(count > 4){
+            isGameOver()
+            
+        }
+        if(count == 9 && !isGameOver())
+        out.innerText = "GAME DRAW"
     }
 }
